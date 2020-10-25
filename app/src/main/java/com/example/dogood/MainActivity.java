112c8adfd5d7c6452ab5b1682db5ba3448d9f007
Nathan Amiel <NathanAmiel5@gmail.com>
2020-10-25 15:05:06 +0200

    start

--- app/src/main/java/com/example/dogood/MainActivity.java
+++ app/src/main/java/com/example/dogood/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.dogood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "Dogood";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: ");
     }
 }
