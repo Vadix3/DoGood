c68ca58385b792a76a52b8d040f991001425a7da
Vadim <Vadix3@gmail.com>
2020-10-25 19:30:13 +0200

    test

--- app/src/main/java/com/example/dogood/MainActivity.java
+++ app/src/main/java/com/example/dogood/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.view.menu.MenuBuilder;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.app.FragmentManager;
@@ -16,8 +17,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.dogood.fragments.MainListFragment;
+import com.example.dogood.objects.GiveItem;
+import com.example.dogood.objects.RequestItem;
+import com.example.dogood.objects.User;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private static final String TAG = "Dogood";
@@ -26,14 +33,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private DrawerLayout main_LAY_main;
     private NavigationView main_NGV_side;
 
+    private ArrayList<GiveItem>giveItems;
+    private ArrayList<RequestItem>requestItems;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate: Ya gever");
 
         findViews();
         addSide();
+
+
+        /**TESTING ARRAYS*/
+        initTestArrays();
+
+
+
+        initItemsFragment();
     }
 
     private void findViews() {
@@ -92,5 +109,45 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } else {
             super.onBackPressed();
         }
+    }
+
+
+    /**
+     * A method to create test arrays
+     */
+    private void initTestArrays() {
+        Log.d(TAG, "initTestArrays: Creating test arrays");
+        User testUser = new User("Vadim", "dogoodapp1@gmail.com", "123456"
+                , "Netanya", "0541234567", "Photo URL");
+
+        giveItems = new ArrayList<>();
+        requestItems = new ArrayList<>();
+
+        requestItems.add(new RequestItem("12345", "Refrigirator", "Appliances", "Tel Aviv"
+                , "A simple white fridge", "21/09/2020", testUser, false));
+        requestItems.add(new RequestItem("12345", "Microwave", "Appliances", "Petah Tikva"
+                , "A simple white microwave", "21/09/2020", testUser, false));
+
+
+        giveItems.add(new GiveItem("123123", "Computer", "Computers", "New"
+                , "Free", "A 2 year old working computer", "Photo URL", "23/04/20", testUser));
+        giveItems.add(new GiveItem("123123", "Oven", "Appliances", "New"
+                , "Free", "De Longhi oven", "Photo URL", "13/05/20", testUser));
+        giveItems.add(new GiveItem("123123", "Television", "Electronics", "New"
+                , "Free", "LG 50' tv", "Photo URL", "11/01/20", testUser));
+        giveItems.add(new GiveItem("123123", "Coffee Maker", "Electronics", "New"
+                , "Free", "Nespresso", "Photo URL", "11/01/20", testUser));
+
+    }
+
+    /**
+     * A method to init the main list
+     */
+    private void initItemsFragment() {
+        Log.d(TAG, "initItemsFragment: Initing main list");
+        MainListFragment mainListFragment = new MainListFragment(giveItems, requestItems);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_LAY_recyclerFrame, mainListFragment);
+        transaction.commit();
     }
 }
