903765dfd656879fa62c6067dd8ffdbd0c0d3119
Nathan Amiel <NathanAmiel5@gmail.com>
2020-10-27 00:25:44 +0200

    create profile page

---
+++ app/src/main/java/com/example/dogood/activities/Activity_profile.java
@@ -0,0 +1,15 @@
+package com.example.dogood.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.dogood.R;
+
+public class Activity_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
